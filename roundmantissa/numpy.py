2397d2e5ec915b4e9f5e17d33696abd56caccde2
Mario Geiger <geiger.mario@gmail.com>
2022-08-25 11:45:45 -0400

    numpy.logspace

--- roundmantissa/numpy.py
+++ roundmantissa/numpy.py
@@ -30,3 +30,22 @@ def round_mantissa(x: np.ndarray, n: int) -> np.ndarray:
         return s * x
 
     return np.where(x == 0.0, 0.0, fn(np.where(x == 0.0, 1.0, x)))
+
+
+def logspace(start: int, stop: int, n: int) -> np.ndarray:
+    """Logarithmically spaced array between ``2**start`` and ``2**stop``.
+
+    Args:
+        start: starting exponent in base 2
+        stop: ending exponent in base 2
+        n: number of mantissa digits to keep
+
+    Returns:
+        logarithmically spaced array
+    """
+    m = np.stack(
+        np.meshgrid(*[np.array([0.0, 1.0])] * n, indexing="ij"), axis=-1
+    ).reshape(-1, n)
+    m = 1.0 + np.sum(m * 0.5 ** (np.arange(n) + 1.0), axis=-1)
+    x = m * 2 ** np.arange(start, stop)[:, None]
+    return np.concatenate([x.reshape(-1), [2**stop]])
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from roundmantissa import round_mantissa
 from roundmantissa.numpy import round_mantissa as round_mantissa_numpy
+from roundmantissa.numpy import logspace as logspace_numpy
 
 assert round_mantissa(5.0, 0) == 4.0
 assert round_mantissa(5.0, 1) == 4.0
@@ -12,4 +13,15 @@ assert round_mantissa(0.50123, 2) == 0.5
 np.testing.assert_allclose(
     round_mantissa_numpy(np.array([0.0, 0.50123, 0.5 + 0.25 + 0.125]), 2),
     np.array([0.0, 0.5, 0.875]),
+)
+
+start, stop, n = 2, 4, 3
+np.testing.assert_allclose(
+    np.unique(
+        round_mantissa_numpy(
+            np.logspace(np.log10(2**start), np.log10(2**stop), 1000),
+            n,
+        )
+    ),
+    logspace_numpy(start, stop, n),
 )
